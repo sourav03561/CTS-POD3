0b61ab2c25f4b4cf00570f30970ba32eb83945b9
Sourav Sarkar <46227372+sourav03561@users.noreply.github.com>
2021-06-13 13:01:12 +0530

    Update MedicineStockControllerTest.java
    All unit test added

--- Pharmacy-Supply-Management-System/PSMS-Medicine-Stock-Microservice/src/test/java/MedicineStockControllerTest.java
+++ Pharmacy-Supply-Management-System/PSMS-Medicine-Stock-Microservice/src/test/java/MedicineStockControllerTest.java
@@ -1,40 +1,94 @@
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.*;
 
-import org.junit.Before;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import com.cts.pmsm.controller.MedicineStockController;
+import com.cts.pmsm.exception.MedicineStockNotFoundException;
+import com.cts.pmsm.model.MedicineStock;
+import com.cts.pmsm.service.StockServiceImpl;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import lombok.extern.slf4j.Slf4j;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+@AutoConfigureMockMvc
 public class MedicineStockControllerTest {
 
-	private MockMvc mockMvc;
-	
 	@InjectMocks
-	private MedicineStockController medicine;
+	private MedicineStockController medicineStockController;
 	
-	@Before
-	public void setUp() throws Exception{
-		mockMvc=MockMvcBuilders.standaloneSetup(medicine).build();
+	@Mock
+	private MedicineStock medicineStock;
+	
+	@Mock
+	private StockServiceImpl stockService;
+	
+	@Test
+	public void testGetStockInformation() {
+		List<MedicineStock> list=medicineStockController.getStockInformation();
+		assertNotNull(list);
 	}
 	
 	@Test
-	public void testHealthCheck() throws Exception {
-		
-		mockMvc.perform(
-				MockMvcRequestBuilders.get("/healthCheck")
-				)
-		   .andExpect(status().isOk())
-		   .andExpect(content().string("working"));
-		
+	public void testGetStockById()
+	{
+		try
+		{
+			ResponseEntity<?> getStock=medicineStockController.getStockById(1);
+			HttpStatus statusCode=getStock.getStatusCode();
+			assertNotNull(statusCode);
+			assertEquals(statusCode,HttpStatus.OK);
+		}
+		catch(MedicineStockNotFoundException e)
+		{}
 	}
 	
+	@Test
+	public void testCreateMedicineStock()
+	{
+		LocalDate date=LocalDate.of(2022, 9, 8);
+		MedicineStock obj = new MedicineStock(8,"Ivepred","Methyl Predisolone","Steroid",date,250);
+		ResponseEntity<?> createMedicine=medicineStockController.createMedicineStock(obj);
+		HttpStatus statusCode=createMedicine.getStatusCode();
+		assertNotNull(statusCode);
+		assertEquals(statusCode,HttpStatus.OK);
+	}
+	
+	@Test
+	public void testDeleteStockById()
+	{
+		ResponseEntity<?> deleteMedicine=medicineStockController.deleteStockById(7);
+		HttpStatus statusCode=deleteMedicine.getStatusCode();
+		assertNotNull(statusCode);
+		assertEquals(statusCode,HttpStatus.OK);
+	}
+	
+	@Test
+	public void testUpdateStockById()
+	{
+		try
+		{
+			LocalDate date=LocalDate.of(2022, 9, 8);
+			MedicineStock obj = new MedicineStock(8,"Ivepred 8","Methyl Predisolone","Steroid",date,250);
+			ResponseEntity<?> updateMedicine=medicineStockController.updateStockById(8, obj);
+			HttpStatus statusCode=updateMedicine.getStatusCode();
+			assertNotNull(statusCode);
+			assertEquals(statusCode,HttpStatus.OK);
+		}
+		catch(MedicineStockNotFoundException e) {}
+	}
+
 }
