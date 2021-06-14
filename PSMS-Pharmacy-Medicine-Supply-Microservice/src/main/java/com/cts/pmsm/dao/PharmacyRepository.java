d0c1276f617faef65829f885a304fffa0bded20b
rishavbhadra <53603103+rishavbhadra@users.noreply.github.com>
2021-06-14 16:55:44 +0530

    Delete PSMS-Pharmacy-Medicine-Supply-Microservice directory

--- PSMS-Pharmacy-Medicine-Supply-Microservice/src/main/java/com/cts/pmsm/dao/PharmacyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.pmsm.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.pmsm.model.Pharmacy;
-
-public interface PharmacyRepository extends JpaRepository<Pharmacy, Integer>{
-
-}
