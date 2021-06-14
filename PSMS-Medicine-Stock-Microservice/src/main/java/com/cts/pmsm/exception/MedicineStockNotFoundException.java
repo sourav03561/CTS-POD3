645ab79e7ca1df9adbed1467695fef34cd8401ab
rishavbhadra <53603103+rishavbhadra@users.noreply.github.com>
2021-06-14 17:08:56 +0530

    Delete PSMS-Medicine-Stock-Microservice directory

--- PSMS-Medicine-Stock-Microservice/src/main/java/com/cts/pmsm/exception/MedicineStockNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.cts.pmsm.exception;
-
-public class MedicineStockNotFoundException extends Exception{
-	
-	public MedicineStockNotFoundException(String message) {
-		super(message);
-	}
-
-}
