7502f9e420186815772e6cb15b1c4d75c09cec73
rishavbhadra <53603103+rishavbhadra@users.noreply.github.com>
2021-06-15 15:48:42 +0530

    Add files via upload

---
+++ Pharmacy-Supply-Management-System/PSMS-Common-Module/src/main/java/com/cts/pmsm/exception/MedicineStockNotFoundException.java
@@ -0,0 +1,12 @@
+package com.cts.pmsm.exception;
+
+public class MedicineStockNotFoundException extends Exception {
+
+	public MedicineStockNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	public MedicineStockNotFoundException(String message) {
+		super(message);
+	}
+
+}
