550c6a4c4917b56eed514d2e1eb462a36ecc86df
Sourav Sarkar <46227372+sourav03561@users.noreply.github.com>
2021-06-17 11:57:35 +0530

    Delete Pharmacy-Supply-Management-System directory

--- Pharmacy-Supply-Management-System/PSMS-Pharmacy-Medicine-Supply-Microservice/src/main/java/com/cts/pmsm/exception/AuthorizationException.java
+++
@@ -1,9 +0,0 @@
-package com.cts.pmsm.exception;
-
-@SuppressWarnings("serial")
-public class AuthorizationException extends Exception {
-
-	public AuthorizationException(String message) {
-		super(message);
-	}
-}
