fc9cd7acf40abf638fd21ad04f0133eba081b0c9
adarshasonai <80759786+adarshasonai@users.noreply.github.com>
2021-06-17 18:55:40 +0530

    Add files via upload

---
+++ Pharmacy-Supply-Management-System-MMV/PSMS-Common-Module/src/main/java/com/cts/pmsm/CommonModuleApplication.java
@@ -0,0 +1,10 @@
+package com.cts.pmsm;
+
+import org.springframework.boot.SpringApplication;
+
+
+public class CommonModuleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CommonModuleApplication.class, args);
+	}
+}
